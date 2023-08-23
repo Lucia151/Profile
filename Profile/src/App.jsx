57a963877e44c221ce5ebbf10ff92b941e2e8410
zizile <zizilelucia@gmail.com>
2023-08-23 16:50:07 +0200

    changes

--- Profile/src/App.jsx
+++ Profile/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import About from "./Components/About"
 import Project from "./Components/Projects"
 import Home from "./Components/Landing"
+import NavBar from './Components/NavBar'
 
 function App() {
 
   return (
     <>
+    <NavBar/>
     <Home/>
     <About/>
     
