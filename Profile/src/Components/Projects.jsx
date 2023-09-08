3903ae98ad3fb12898d5c2f066e57b3d8f12b2d9
zizile <zizilelucia@gmail.com>
2023-09-08 04:14:46 +0200

    adding cards, installed nesting plugin

--- Profile/src/Components/Projects.jsx
+++ Profile/src/Components/Projects.jsx
@@ -10,56 +10,56 @@ export default function Projects() {
   return (
     
     <div className="projects">
-      <a href="https://github.com/Lucia151/TaxCalculation" rel="noopener noreferrer" target="_blank">
-        
-        <img className="Tax" alt="Rectangle" src={imageT}/>
-      </a>
-      <div className="frame">
-        <img className="chat" alt="Rectangle" src={imageC} />
+  {/* Card 1 */}
+  <div className="card">
+    <a href="https://github.com/Lucia151/TaxCalculation" rel=" " target="_blank">
+      <img className="card-image" alt="Tax Calculator" src={imageT} />
+    </a>
+    <div className="card-text">
+      <div className="text-wrapper">Java</div>
+      <div className="text-wrapper-9">Tax Calculator</div>
+      <div className="div-wrapper">
+        <div className="text-wrapper-10">HTML</div>
       </div>
-      <a href="https://github.com/Lucia151/Bank-app" rel="noopener noreferrer" target="_blank">
-      <img className="bank" alt="Rectangle" src={imageb} />
-      </a>
-      <div className="overlap">
-        <div className="overlap-group">
-          <div className="text-wrapper">Java</div>
-        </div>
-        <div className="div">Bank App</div>
-        <div className="div-wrapper">
-          <div className="text-wrapper-2">HTML</div>
-        </div>
-        <div className="overlap-2">
-          <div className="text-wrapper-3">CSS</div>
-        </div>
+      <div className="overlap-9">
+        <div className="text-wrapper-11">CSS</div>
       </div>
-      <div className="overlap-3">
-        <div className="text-wrapper-4">Chat App</div>
-        <div className="overlap-4">
-          <div className="text-wrapper-5">JavaScript</div>
-        </div>
-        <div className="overlap-5">
-          <div className="text-wrapper-6">HTML</div>
-        </div>
-        <div className="overlap-6">
-          <div className="text-wrapper-7">CSS</div>
-        </div>
-      </div>
-      <div className="overlap-group-2">
-        <div className="overlap-7">
-          <div className="text-wrapper-8">Python</div>
-        </div>
-        <div className="text-wrapper-9">Tax Calculator</div>
-        <div className="overlap-8">
-          <div className="text-wrapper-10">HTML</div>
-        </div>
-        <div className="overlap-9">
-          <div className="text-wrapper-11">CSS</div>
-        </div>
-      </div>
-      {/* <img className="download-removebg" alt="Download removebg" src="download-1-removebg-preview-4.png" />
-      <img className="download-removebg-2" alt="Download removebg" src="download-1-removebg-preview-3.png" /> */}
-      <Myfooter/>
     </div>
+  </div>
+
+  {/* Card 2 */}
+  <div className="card">
+    <a href="https://github.com/Lucia151/Bank-app" rel="noopener noreferrer" target="_blank">
+      <img className="card-image" alt="Bank App" src={imageb} />
+    </a>
+    <div className="card-text">
+      <div className="text-wrapper">Java</div>
+      <div className="text-wrapper-2">Bank App</div>
+      <div className="div-wrapper">
+        <div className="text-wrapper-10">HTML</div>
+      </div>
+      <div className="overlap-2">
+        <div className="text-wrapper-3">CSS</div>
+      </div>
+    </div>
+  </div>
+
+  {/* Card 3 */}
+  <div className="card">
+    <a href="#" rel="noopener noreferrer" target="_blank">
+      <img className="card-image" alt="Chat App" src={imageC} />
+    </a>
+    <div className="card-text">
+      <div className="text-wrapper-5">JavaScript</div>
+      <div className="text-wrapper-6">HTML</div>
+      <div className="overlap-6">
+        <div className="text-wrapper-7">CSS</div>
+      </div>
+    </div>
+  </div>
+</div>
+
  
   )
 }
+
